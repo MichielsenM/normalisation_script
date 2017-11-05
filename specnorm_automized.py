be8405c3da8dfd6b70a2ca8dd84aa74f018ff65b
Niels Claes <niels.claes@live.be>
2017-11-05 12:14:45 +0100

    Modified script, improved function calling

--- specnorm_automized.py
+++ specnorm_automized.py
@@ -6,9 +6,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.constants as cte
 
-from scipy.interpolate import splrep,splev
+from scipy.interpolate import splrep, splev
 from astropy.io import fits
-
+from scipy import interpolate
 
 # from http://python4esac.github.io/plotting/specnorm.html
 
@@ -24,20 +24,70 @@ def read_fits(fname):
         flux = hdu[0].data
         wave = np.arange(hdr['naxis1']) * hdr['cdelt1'] + hdr['crval1']
         bvcor = hdr['BVCOR']
-        return bvcor, wave, flux
-    
+        SNR = hdr['SNR50']
+        return SNR, bvcor, wave, flux
+
 def onclick(event):
-    # when none of the toolbar buttons is activated and the user clicks in the
-    # plot somewhere, compute the median value of the spectrum in a .5 angstrom
-    # window around the x-coordinate of the clicked point. The y coordinate
-    # of the clicked point is not important. Make sure the continuum points
-    # `feel` it when it gets clicked, set the `feel-radius` (picker) to 5 points
+    """
+    When no toolbar buttons are activated and the user clicks in the plot
+    somewhere, compute median value of spectrum in a 0.5 A window around
+    the x-coordinate of the clicked point. The y-coordinate of the clicked
+    point is not important. The feel-radius (picker) is set to 5 point.
+    :param event: User event
+    :return: Continuum point added to graph when clicking left.
+    """
     toolbar = plt.get_current_fig_manager().toolbar
     if event.button==1 and toolbar.mode=='':
         window = ((event.xdata-.25)<=wave) & (wave<=(event.xdata+.25))
         y = np.median(flux[window])
-        plt.plot(event.xdata,y,'rs',ms=10,picker=5,label='cont_pnt')
+        plt.plot(event.xdata,y,'ro',ms=5,picker=5,label='cont_pnt')
     plt.draw()
+
+def drawContinuumPoint(pointList, markerColor):
+    """
+    Draws a continuum point at a specific location.
+    :param pointList: List of continuum points containing tuples [wavelength, flux]
+    :param markerColor: Color of marker to draw
+    :return: Continuum points drawn on the current graph.
+    """
+    for pt in pointList:
+        plt.plot(pt[0], pt[1], 'o', color=markerColor, ms=5, picker=5, label='cont_pnt')
+        plt.draw()
+
+def find_nearest(array,value):
+    """
+    Finds nearest value in a given array.
+    :param array: The corresponding Numpy array.
+    :param value: The value to search for.
+    :return: The index of the nearest corresponding value in the given array.
+    """
+    idx = (np.abs(array-value)).argmin()
+    return idx
+
+def automized_search(bin_width):
+    rIdx = len(wave)-1
+    nbBins = 1
+    pointList = []
+
+    while rIdx > 0:
+        lIdx = find_nearest(wave, wave[rIdx] - bin_width)
+        bin_flux = flux[lIdx:rIdx+1]
+        bin_wave = wave[lIdx:rIdx+1]
+        if (np.isnan(bin_flux).any() or np.isnan(bin_wave).any()):
+            print "Nan value encountered in bin %s: interval skipped and continuing." % nbBins
+            rIdx = lIdx
+            nbBins += 1
+            continue
+        ctn_flux = np.nanpercentile(bin_flux, 85)
+        # ctn_wave = bin_wave[find_nearest(bin_flux, ctn_flux)]
+        ctn_wave = wave[rIdx] - bin_width / 2.
+        pointList.append([ctn_wave, ctn_flux])
+        rIdx = lIdx
+        nbBins += 1
+
+    drawContinuumPoint(pointList, 'red')
+    plt.show()
+
 
 def onpick(event):
     # when the user clicks right on a continuum point, remove it
@@ -46,6 +96,13 @@ def onpick(event):
             event.artist.remove()
 
 def ontype(event):
+    # When the user hits a:
+    # 1. Start on the right side of the spectrum, as HERMES is more accurate there
+    # 2. Determine ideal continuum line
+    # 3. Draw points
+    if event.key=='a':
+        automized_search(20)
+
     # when the user hits enter:
     # 1. Cycle through the artists in the current axes. If it is a continuum
     #    point, remember its coordinates. If it is the fitted continuum from the
@@ -70,7 +127,6 @@ def ontype(event):
     # when the user hits 'n' and a spline-continuum is fitted, normalise the
     # spectrum
     elif event.key=='n':
-        global flux, wave    
         continuum = None
         for artist in plt.gca().get_children():
             if hasattr(artist,'get_label') and artist.get_label()=='continuum':
@@ -79,10 +135,8 @@ def ontype(event):
         if continuum is not None:
             plt.cla()
             plt.plot(wave,flux/continuum,'k-',label='normalised')
-            
             plt.axhline(1.01)
             plt.axhline(0.99)
-            flux = flux/continuum
 
     # when the user hits 'r': clear the axes and plot the original spectrum
     elif event.key=='r':
@@ -95,68 +149,20 @@ def ontype(event):
         for artist in plt.gca().get_children():
             if hasattr(artist,'get_label') and artist.get_label()=='normalised':
                 data = np.array(artist.get_data())
-                np.savetxt(os.path.splitext(filename)[0]+'_norm.dat',data.T)
+                #np.savetxt(os.path.splitext(filename)[0]+'_norm.dat',data.T)
+                np.savetxt(saveName + '_norm.dat', data.T)
                 print('Saved to file')
                 break
-    
-    # when the user hits 'a': automatically divide spectrum in bins and search for continuum point in each bin
-    elif event.key=='a':        
-        global cont_wave_list, cont_flux_list
-        automized_search(wave, flux, 20)
-        for i in range(0, len(cont_flux_list)):
-            plt.plot(cont_wave_list[i],cont_flux_list[i],'rs',ms=10,picker=5,label='cont_pnt')    
-        cont_wave_list = []
-        cont_flux_list = []
-
-    elif event.key=='q':
-        global cont_wave_list, cont_flux_list
-        automized_search(wave, flux, 100)
-        for i in range(0, len(cont_flux_list)):
-            plt.plot(cont_wave_list[i],cont_flux_list[i],'rs',ms=10,picker=5,label='cont_pnt')   
-        cont_wave_list = []
-        cont_flux_list = []            
     plt.draw()
 
-# Find the index of the value, closest to the specified value in a given array.
-def find_nearest(array, value):
-    idx = (np.abs(array - value)).argmin()
-    return idx
 
-def automized_search(wave, flux, bin_width):
-    i = len(wave)-1
-    while i > 0:
-        low_edge = wave[i]-bin_width
-        low_index= find_nearest(wave, low_edge)
-        
-        bin_wave = []
-        bin_flux = []
-        for j in range(low_index, i):
-            bin_flux.append(flux[j])
-            bin_wave.append(wave[j])
-        bin_flux = np.asarray(bin_flux)
-        
-        cont_flux = np.percentile(bin_flux, 85)
-        index = find_nearest(bin_flux, cont_flux)
-        cont_wave = bin_wave[index]
-        
-        cont_flux_list.append(cont_flux)
-        cont_wave_list.append(cont_wave)     
-            
-        start_new_bin = cont_wave - 10 # leave 10 angstrom between a continuum point and the start of the next bin
-        i = find_nearest(wave, start_new_bin)
-        
-#def ignore_lines(wave, flux)
-    #nr_points = 300
-    ##for i in range(0, nr_points):
-        
-    ##for i in range(nr_points, len(wave)-nr_points):
-    
-    #for i in range(0, len(wave)):
-        #if i < nr_points:
-            
-        #elif i>len(wave)-nr_points:
-        
-        #else:
+def add_exposures(SNR1, input_wvl_1, input_flux_1, SNR2, input_wvl_2, input_flux_2):
+    flux_composed_spectrum = (input_flux_1 + input_flux_2)/2
+    wavelength_composed_spectrum = input_wvl_1
+    SNR = (SNR1+SNR2)/2 * np.sqrt(2)
+    return SNR, wavelength_composed_spectrum, flux_composed_spectrum
+
+
 def barycentric_correction(bvcor, wvl, flx):
     print "Applying barycentric correction."
     print "Value found in fits file: BVCOR =", bvcor, " km/s"
@@ -171,34 +177,44 @@ def barycentric_correction(bvcor, wvl, flx):
 
 
 if __name__ == "__main__":
+    print "--- Running automated_normalisation.py ---"
+    compose = False
     bary_corr = True
-    
-    global flux, wave, cont_wave_list, cont_flux_list
-    cont_flux_list = []
-    cont_wave_list = []    
     # Get the filename of the spectrum from the command line, and plot it
-    filename = sys.argv[1]
-    if filename.endswith('fits') or filename.endswith('fit'):
-        bvcor, wave, flux = read_fits(filename)
-    else:
-        wave, flux = np.loadtxt(filename).T[:2]
-    spectrum, = plt.plot(wave,flux,'k-',label='spectrum')
-    plt.title(filename)
-    
-    if bary_corr:
-        wave, flux = barycentric_correction(bvcor, wave, flux)
-        
-    wave_nanremoved = []
-    flux_nanremoved = []
-    for i in range(0, len(wave)):
-        if not np.isnan(flux[i]):
-            flux_nanremoved.append(flux[i])
-            wave_nanremoved.append(wave[i])
-            
-    flux = np.asarray(flux_nanremoved)
-    wave = np.asarray(wave_nanremoved)
+    #filename = sys.argv[1]
+    if not compose:
+        saveName = "00851884"
+        filename = "data/00851884_HRF_OBJ_ext_CosmicsRemoved_wavelength_merged_c.fits"
 
-        
+        SNR, bvcor, wave, flux = read_fits(filename)
+		
+        print "SNR of spectrum:", SNR
+
+        if bary_corr:
+            wave, flux = barycentric_correction(bvcor, wave, flux)
+
+        spectrum, = plt.plot(wave,flux,'k-',label='spectrum')
+        plt.title(saveName)
+    else:
+        saveName = "SAVENAME"
+        filename = "PATH1"
+        filename2 = "PATH2"
+
+        SNR1, bvcor1, wave1, flux1 = read_fits(filename)
+        SNR2, bvcor2, wave2, flux2 = read_fits(filename2)
+
+        if bary_corr:
+            wave1_corr, flux1_corr = barycentric_correction(bvcor1, wave1, flux1)
+            wave2_corr, flux2_corr = barycentric_correction(bvcor2, wave2, flux2)
+            SNR, wave, flux = add_exposures(SNR1, wave1_corr, flux1_corr, SNR2, wave2_corr, flux2_corr)
+        else:
+            SNR, wave, flux = add_exposures(SNR1, wave1, flux1, SNR2, wave2, flux2)
+
+        print "SNR of composed spectrum:", SNR
+
+        spectrum, = plt.plot(wave, flux, 'k-', label='spectrum')
+        plt.title("Composed spectrum")
+
     # Connect the different functions to the different events
     plt.gcf().canvas.mpl_connect('key_press_event',ontype)
     plt.gcf().canvas.mpl_connect('button_press_event',onclick)
